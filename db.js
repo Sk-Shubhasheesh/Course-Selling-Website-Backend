e527327557b22c1362bd0afeeec83064cf5ad987
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-01-13 11:32:13 +0530

    Added the Schema

---
+++ db.js
@@ -0,0 +1,50 @@
+const mongoose =require('mongoose');
+console.log("DbConnected");
+
+mongoose.connect("mongodb+srv://shubhasheeshkundu02:OCuA34UcQ390T3YB@cluster0.d2dq8.mongodb.net/CourseSellingBackend");
+
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName:String,
+    lastName:String,
+
+});
+
+const adminSchema = new Schema({
+    email: {type: String, unique: true},
+    password: String,
+    firstName:String,
+    lastName:String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchesSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId,
+
+
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchesModel = mongoose.model("purches", purchesSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchesModel
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const { Router } = require("express")
+const { adminModel } = require('../db.js')
 
 const adminRouter = Router();
 adminRouter.post("/signup", function(req,res){
